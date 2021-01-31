02d810a7f19e419ff527733cb36d7d32ea89ed97
dhmec <dharmoradiya@gmail.com>
2021-01-31 12:59:41 -0700

    Added async function to fetch data from API and Used useEffect hook to prevent every render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
-import  React from "react";
+import  React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+    const [clientData, setClientData] = useState([]); 
+    const [loading, setLoading] = useState(true); 
 
 
 
 
+    const populateClientData = async () => { /*Populates response with API*/
+        const response = await axios.get('https://api.hatchways.io/assessment/students');
+        setClientData(response.data);
+        setLoading(false);
+    }
 
-
+    useEffect(() => { /*Prevent useEffect From Running Every Render*/
+        populateClientData();
+    }, [loading]);
 
     return(
-        <>
+        <>{console.log(clientData)}
         </>
     );
 }
