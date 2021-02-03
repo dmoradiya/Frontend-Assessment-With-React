352428e1e288541ea1dfa19b57bd63b2dbffde44
dhmec <dharmoradiya@gmail.com>
2021-02-03 12:24:03 -0700

    Added functionality to show or hide test info  section based on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import  React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../css/main.css";
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 const App = () => {
     const [clientData, setClientData] = useState([]); 
     const [loading, setLoading] = useState(true); 
-    const [searchValue, setSearchValue] = useState(''); 
+    const [searchValue, setSearchValue] = useState('');
+    const [expand, setExpand] = useState([]); 
 
     const renderClientData = (clientData) => {
         return (
@@ -35,7 +36,9 @@ const App = () => {
                         <p>{"Company : "+val.company}</p>
                         <p>{"Skill : "+val.skill}</p>                    
                         <p>{"Average : "+val.grades.map((x,i,arr) => x/arr.length).reduce((a,b) => a + b)+"%"}</p>               
-                        <div>
+                        {uniqueItems.map(item => (item === val.id) ?                         
+                        
+                        <div className={clickCounter(expand,val.id) ? "show-results" : "hide-results"} >
                             <p>{"Test 1 : "+val.grades[0]}</p>
                             <p>{"Test 2 : "+val.grades[1]}</p>
                             <p>{"Test 3 : "+val.grades[2]}</p>
@@ -44,9 +47,12 @@ const App = () => {
                             <p>{"Test 6 : "+val.grades[5]}</p>
                             <p>{"Test 7 : "+val.grades[6]}</p>
                             <p>{"Test 8 : "+val.grades[7]}</p>
-                        </div>
+                        </div> : 
+                        null
+                        
+                        )}
                     </div>  
-                    <p id="expand-view"><FaPlus /></p>                  
+                    <p className="expand-view" onClick={testInfo(val.id)}>{clickCounter(expand,val.id)? <FaMinus /> : <FaPlus />}</p>       
                 </section>
                 )}
            </section>
@@ -55,7 +61,7 @@ const App = () => {
 
 
 
-    function handleFieldChange(event) { /*Updates the constant values with whatever is located in the input fields*/
+    function handleFieldChange(event) { // Updates the constant values with whatever is located in the input fields
         switch (event.target.id) {
             case "search-by-name-input":
                 setSearchValue(event.target.value);
@@ -65,14 +71,32 @@ const App = () => {
             }
     }
 
-
-    const populateClientData = async () => { /*Populates response with API*/
+    const testInfo = val => (event) => { // Creates ID array based on onclick event
+        event.preventDefault();   
+        setExpand([...expand, val]);
+        
+    }
+   
+    let uniqueItems = [...new Set(expand)] // Creates Distinct element in new array 
+       
+    const clickCounter = (arr, val) => { // Count no. of Clicks 
+        const number = arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+        if (number % 2 === 0)
+        {
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+    const populateClientData = async () => { // Populates response with API
         const response = await axios.get('https://api.hatchways.io/assessment/students');
         setClientData(response.data);
         setLoading(false);
     }
 
-    useEffect(() => { /*Prevent useEffect From Running Every Render*/
+    useEffect(() => { // Prevent useEffect From Running Every Render
         populateClientData();
     }, [loading]);
 
